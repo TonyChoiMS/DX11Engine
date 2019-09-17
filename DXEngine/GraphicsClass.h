5c04a718a590aaa7234a06096df96b81980a20b4
TonyChoi <cms9286@gmail.com>
2019-09-18 00:50:20 +0900

    Initialize D3DDevice
    Initialize D3DDevice

--- DXEngine/GraphicsClass.h
+++ DXEngine/GraphicsClass.h
@@ -3,10 +3,12 @@
 /////////////
 // GLOBALS //
 /////////////
-const bool FULL_SCREEN = true;
+const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
+
+class D3DClass;
 
 // Graphics 클래스 역할 : 모든 그래픽 기능을 이 클래스에 캡슐화됩니다. 
 // 이 파일의 헤더를 전체 화면 또는 창 모드와 같이 변경할 수 있는 모든 그래픽 관련 전역 설정에 사용합니다.
@@ -24,4 +26,7 @@ public:
 
 private:
 	bool Render();
+
+private:
+	D3DClass* m_Direct3D = nullptr;
 };
